d3aa304eb98d013b478b57ead27f40373fd026b3
Patrick <Patrick@nytyler.com>
2025-01-08 20:11:06 -0500

    Api structure

--- api/serve.go
+++ api/serve.go
@@ -1,17 +1,21 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	log "github.com/sirupsen/logrus"
 )
 
-func serve() {
+func Serve() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("classy api running"))
 	})
-	http.ListenAndServe(":3000", r)
+	port := 3000
+	log.Infof("Running server on :%d", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), r)
 }
---
+++ cmd/serve.go
@@ -0,0 +1,23 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"github.com/Pjt727/classy/api"
+	"github.com/spf13/cobra"
+)
+
+// serveCmd represents the serve command
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Runs the api service",
+	Long:  `Runs the api service`,
+	Run: func(cmd *cobra.Command, args []string) {
+		api.Serve()
+	},
+}
+
+func init() {
+	appCmd.AddCommand(serveCmd)
+}
